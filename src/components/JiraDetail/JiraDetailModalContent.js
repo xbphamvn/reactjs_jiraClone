6bab68fd7f00de4a59aee04a2fb862cd4f0443a7
XB Pham <xbphamvn@gmail.com>
2021-01-30 01:01:35 +0700

    Update task detail a little bit!

--- src/components/JiraDetail/JiraDetailModalContent.js
+++ src/components/JiraDetail/JiraDetailModalContent.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Select, Slider } from 'antd';
+import ReactHtmlParser from 'react-html-parser';
 
 export default function JiraDetailModalContent(props) {
 
-    const { taskId, taskTypeDetail, taskName } = useSelector(state => state.JiraHOCModalReducer.taskData);
+    const { taskTypeDetail, taskId, taskName, description, assigness } = useSelector(state => state.JiraHOCModalReducer.taskData);
     const { priorityIcon, taskTypeIcon } = useSelector(state => state.JiraDetailTaskItemReducer);
 
-    // console.log(taskData);
+    const { taskData } = useSelector(state => state.JiraHOCModalReducer);
+    console.log(taskData);
 
     return (
         <div className="row container-fluid px-1">
@@ -18,7 +20,12 @@ export default function JiraDetailModalContent(props) {
                 </p>
                 <h4>{taskName}</h4>
                 <p className="fw-bold">Description</p>
-
+                <div className="col-12">
+                    {ReactHtmlParser(description)}
+                </div>
+                <div className="col-12">
+                    <p className="fw-bold">Comments</p>
+                </div>
             </div>
             <div className="col-4">
                 <p className="text-end">
@@ -32,40 +39,34 @@ export default function JiraDetailModalContent(props) {
                     style={{ width: '100%' }}
                     placeholder="Select a person"
                     optionFilterProp="children"
+                    value={'STATUS'}
                 // onChange={onChange}
                 >
                     <Select.Option>123</Select.Option>
                 </Select>
                 <p className="fw-bold mt-3 mb-2">ASSIGNEES</p>
-                <Select
-                    mode="multiple"
-                    name="listUserAsign"
-                    allowClear
-                    // options={memberArr?.map((item) => ({ label: item.name, value: item.userId }))}
-                    style={{ width: '100%' }}
-                    placeholder="Please select"
-                // onChange={(values) => setFieldValue('listUserAsign', values)}
-                />
-                <p className="fw-bold mt-3 mb-2">REPORTER</p>
-                <Select
-                    mode="multiple"
-                    name="listUserAsign"
-                    allowClear
-                    // options={memberArr?.map((item) => ({ label: item.name, value: item.userId }))}
-                    style={{ width: '100%' }}
-                    placeholder="Please select"
-                // onChange={(values) => setFieldValue('listUserAsign', values)}
-                />
+                <div className="pt-1">
+                    {assigness.map((user, index) => ((
+                        <span className="taskDetail__listMember" key={index}>
+                            <img className="taskDetail__listMember--img" src={user.avatar} alt={user.avatar} />
+                            {user.name}
+                            <i className="fal fa-times mx-2" />
+                        </span>
+                    )))}
+                    <span className="taskDetail__listMember--link"><i className="fal fa-plus" /> Add more</span>
+                </div>
                 <p className="fw-bold mt-3 mb-2">PRIORITY</p>
                 <Select
-                    mode="multiple"
-                    name="listUserAsign"
-                    allowClear
-                    // options={memberArr?.map((item) => ({ label: item.name, value: item.userId }))}
+                    showSearch
                     style={{ width: '100%' }}
-                    placeholder="Please select"
-                // onChange={(values) => setFieldValue('listUserAsign', values)}
-                />
+                    placeholder="Select a person"
+                    optionFilterProp="children"
+                    value="2"
+                // onChange={onChange}
+                >
+                    <Select.Option value="2">High</Select.Option>
+                    <Select.Option value="1">Medium</Select.Option>
+                </Select>
                 <p className="fw-bold mt-3 mb-2">ORIGINAL ESTIMATE (HOURS)</p>
                 <input className="form-control text-end" type="number" min="0" defaultValue="0" />
                 <p className="fw-bold mt-3 mb-2">TIME TRACKING</p>
